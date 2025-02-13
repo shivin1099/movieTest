23a602f7e357c187fdc927d83bf6b48dd8fa24d1
shivin1099 <shivindhariwal@hotmail.com>
2025-02-13 18:21:30 +0530

    Added Text

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,11 @@ function App() {
         style={{ backgroundImage: `url(${background})` }}
       >
         <Navbar />
-        <div className="flex justify-center mb-[10%]">
-          <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
+        <div className="flex flex-col items-center">
+          <p className="text-center font-bold text-xl mb-10 text-white font-size:20 mb-[20px]">
+            Track the films you watch and want to watch and get personal recommendations with us bitches
+          </p>
+          <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 mb-[50px]">
             Get Started, It's Free
           </button>
         </div>
